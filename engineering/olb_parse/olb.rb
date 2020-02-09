38c938eeda76bbf3c40dec17672c225e9585ddd0
Gnezd <edwindzeng@gmail.com>
2020-02-09 11:17:08 +0100

    checking in unfinished from MoBiAS rpi

--- engineering/olb_parse/olb.rb
+++ engineering/olb_parse/olb.rb
@@ -1,22 +1,20 @@
-flist = `find /home/ydzeng/mount_points/sq3_c/MassLynx/OALogin/Batchdb/Processed/ -name "*.OLB" -mtime 1`.split("\n")
-#puts flist.length
+#!/usr/bin/ruby
 
-#flist[0..1].each do |x|
-x = "/home/ydzeng/queue/Bode - ycd2097tr2050.OLB"
-	olb = File.open(x, "r")
-	begin
-	while line = olb.readline
-		if line =~/^(NumberOfPlates)/; n_o_p = line.split('=')[1]; break; end
-	end
-	rescue EOFError
-		puts "end of file"
-	end
+class Batch
+	def initialize(olbpath)
+	
+fin = File.open(olbpath, "r")
+raw = fin.readlines
+(0..raw.size-1).each do |ln|
+	if result = raw[ln].chomp.match(/^\[([\d\D]*)\]$/) #if tag
+		puts result[1]
+	end #if tag
 
-	if n_o_p == 1
-		#find the plate and record (PlatePosition)
-		#find the wells and record
-		#push back
-	else
-		find plate 1
+end
+
+	
 	end
-#end
+end
+
+
+a = Batch.new(ARGV[0])
