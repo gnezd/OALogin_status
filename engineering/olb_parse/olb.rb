70bb192b73fc08119e63f893bb4a58b5162b66c2
Gnezd <edwindzeng@gmail.com>
2020-02-09 16:56:14 +0100

    ruby-2.3.3 issue on production rpi: Dir has no method #children

--- engineering/olb_parse/olb.rb
+++ engineering/olb_parse/olb.rb
@@ -6,13 +6,16 @@ olbpath=Dir.open(ARGV[0])
 Dir.chdir(olbpath)
 batchlist = []
 
-criteria = Proc.new {|filename| filename=~/[\djt]\.[Oo][Ll][Bb]$/}
+criteria = Proc.new {|filename| true &&
+		     Time.now-File.mtime(filename) < 8640000
+}
 
-olbpath.children.each do |file|
+puts Dir.glob("*.OLB").size
+Dir.glob("*.OLB").each do |file|
 	next unless criteria[file]
 	batchlist.push(Batch.new(file)) if file =~ /\.OLB$/
 end
 
 
-batchlist.each {|batch| puts batch.batch_param["BatchID"]}
+batchlist.each {|batch| puts batch.batch_param["BatchID"] if batch.plates.size > 1}
 
