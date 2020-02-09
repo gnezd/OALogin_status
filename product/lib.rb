bfa1c5a5930f10d797c806242e78bda81ba98d78
Gnezd@home <edwindzeng@gmail.com>
2020-02-09 15:48:21 +0100

    Move Batch(OLS) parser to main library. Ready to fish out plate # inconsistency from instrument

--- product/lib.rb
+++ product/lib.rb
@@ -155,8 +155,59 @@ class Machine
 	end
 end
 
+class Batch
+	attr_reader :batch_param, :plates, :samples
+	def initialize(olbpath)
+	
+@batch_param = {}
+@plates, @samples = [], []
+ptr = []
+fin = File.open(olbpath, "r")
+raw = fin.readlines
+current_plate=0
+begin
+
+(0..raw.size-1).each do |ln|
+	if result = raw[ln].chomp.match(/^\[([\w]*):?(\d)?:?(\d)?\]$/) #if tag
+#	puts "matching tag #{result[0]}"
+		case result[1]
+		when "Batch"
+			ptr = @batch_param
+		#	puts "batch tag"
+		when "Sample"
+			@samples.push(Hash.new)
+			ptr = @samples.last
+			ptr["plate"], ptr["vial"] = result[2..3]
+			raise "plate # dosn't match with file #{olbpath} at line #{ln}, revise logic!" if ptr["plate"] != current_plate
+		when "Plate"
+			@plates.push(Hash.new)
+			ptr = @plates.last
+			ptr["index"] = current_plate = result[2]
+		end #end tag type
+	elsif result = raw[ln].chomp.match(/([^=]+)=([\w\W]*)/)
+		#puts "#{result[1]} === #{result[2]}"
+		ptr["#{result[1]}"] = result[2]
+	else
+		raise "Unrecognizable line: #{raw[ln]}"
+	end #if tag/line
+
+end
+rescue RuntimeError => err
+	case err.message
+	when /Unrecognizable\sline/
+		puts err
+	when /plate\s#/
+		puts err
+	else
+		raise "Fucked up"
+	end
+end
+	
+	end
+end
+
 #for visualization
-#
+
 def p_curve_plot(path, svg_out, n)
 	rpt_list = `find \"#{path}\" -maxdepth 1 -name \"*.rpt\" -mtime -6 -type f -printf '%Ts %f\n'| sort -nr|head -#{n}|cut -d ' ' -f2-`.split("\n")
 	fname_list = []
