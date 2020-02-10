abf180150307f83feef29c6bd8fd1ae3ff5b9baf
Gnezd <edwindzeng@gmail.com>
2020-02-10 07:26:36 +0100

    find instead of glob due to performance diff

--- engineering/rpt_parse/submission_logbook.rb
+++ engineering/rpt_parse/submission_logbook.rb
@@ -1,26 +1,25 @@
 require './rpt_parse_lib.rb'
-rptpath = Dir.open(ARGV[0])
-Dir.chdir(rptpath)
+fo = File.open("last_24hr.csv", "w")
+#rptpath = Dir.open(ARGV[0])
+#Dir.chdir(rptpath)
 rptlist = []
 
-fo = File.open("last_24hr.csv")
-output = "rpt.name, sample.name, sample.description,sample.acqu_time, sample.lc_method, sample.ms_method, sample.inject_volume"
+fo.puts "rpt.name, sample.name, sample.description,sample.acqu_time, sample.lc_method, sample.ms_method, sample.inject_volume"
 
 
-criteria = Proc.new {|filename| filename =~ /\.rpt$/ ||
+criteria = Proc.new {|filename, rpt| filename =~ /\.rpt$/ &&
 		     Time.now-File.mtime(filename) < 86400
+#		     rpt.samples[0].name =~ /QC/
 }
-Dir.glob("*.rpt").each do |file|
+filelist = `find "#{ARGV[0]}" -mtime -1 -name "*.rpt"`.split("\n")
+puts filelist.size
+filelist.each do |file|
 
-	next unless criteria[file]
 	rpt = OALogin_Report.new(file)
-
-	puts rpt.samples.size
+	next unless criteria[file, rpt]
 	rpt.samples.each do |sample|
-	output += [rpt.name, sample.name, sample.description,sample.acqu_time, sample.lc_method, sample.ms_method, sample.inject_volume].join(",")
-	output += "\n"
-end
+	fo.puts [rpt.name, sample.name, sample.description,sample.acqu_time, sample.lc_method, sample.ms_method, sample.inject_volume].join(",")
+	end
 
 end #file iteration
-fo.puts output
 fo.close
