bed6b5e27a83c12a78b44b2a6f7fa44b332135d9
Gnezd <edwindzeng@gmail.com>
2020-11-27 16:24:48 +0100

    p_curve bug fix

--- product/lib.rb
+++ product/lib.rb
@@ -1,256 +1,254 @@
 #!/usr/bin/ruby
 
+require 'open3'
 require 'time'
 require '../engineering/rpt_parse/rpt_parse_lib.rb'
 
 def display_bytes(str)
-	ret = String.new
-	str.each_byte do  |byte|
-	ret += "%02X|" % byte
-	end
-return ret
+  ret = String.new
+  str.each_byte do |byte|
+    ret += "%02X|" % byte
+  end
+  return ret
 end
 
 def readint(ols, ptr)
-	return ols[ptr..ptr+3].unpack('l')[0]
+  return ols[ptr..ptr + 3].unpack('l')[0]
 end
 
 def read_text_field(ols, ptr, field_name)
+  field_ptr = readint(ols, ptr)
+  ptr += 4
+  raise "Not this field #{field_name} at #{ptr}!" unless ols[ptr..ptr + 3 + field_name.length] == [field_name.length].pack('l') + field_name
 
-	field_ptr = readint(ols, ptr)
-	ptr += 4
-	raise "Not this field #{field_name} at #{ptr}!" unless ols[ptr..ptr+3+field_name.length] == [field_name.length].pack('l')+field_name 
-	field_l = readint(ols, ptr+4+field_name.length)
-	raise "Field length invalid: field_ptr is #{field_ptr} but field length goes to #{ptr+4+field_name.length+4+field_l}" unless field_ptr == ptr+4+field_name.length+4+field_l 
-	ptr = ptr+4+field_name.length+4	
-	field_value = ols[ptr..ptr+field_l-1]
-	ptr = field_ptr
+  field_l = readint(ols, ptr + 4 + field_name.length)
+  raise "Field length invalid: field_ptr is #{field_ptr} but field length goes to #{ptr + 4 + field_name.length + 4 + field_l}" unless field_ptr == ptr + 4 + field_name.length + 4 + field_l
 
-	return field_value, ptr
+  ptr = ptr + 4 + field_name.length + 4
+  field_value = ols[ptr..ptr + field_l - 1]
+  ptr = field_ptr
+
+  return field_value, ptr
 end
 
 def read_int_field(ols, ptr, field_name)
-
-	data, ptr = read_32b(ols, ptr, field_name)
-	return data.unpack('l')[0], ptr
+  data, ptr = read_32b(ols, ptr, field_name)
+  return data.unpack('l')[0], ptr
 end
 
 def read_32b(ols, ptr, field_name)
+  field_ptr = readint(ols, ptr)
+  # puts "field_ptr when reading #{field_name} at #{ptr} points to #{field_ptr}"
+  ptr += 4
+  raise "Not this field #{field_name} at #{ptr}!" unless ols[ptr..ptr + 3 + field_name.length] == [field_name.length].pack('l') + field_name
 
-	field_ptr = readint(ols, ptr)
-	#puts "field_ptr when reading #{field_name} at #{ptr} points to #{field_ptr}"
-	ptr += 4
-	raise "Not this field #{field_name} at #{ptr}!" unless ols[ptr..ptr+3+field_name.length] == [field_name.length].pack('l')+field_name 
-	field_l = 4
-	raise "Field length invalid: field_ptr is #{field_ptr} but field length goes to #{ptr+4+field_name.length+field_l}" unless field_ptr == ptr+4+field_name.length+field_l 
-	ptr = ptr+4+field_name.length	
-	field_value = ols[ptr..ptr+field_l-1]
-	ptr = field_ptr
+  field_l = 4
+  raise "Field length invalid: field_ptr is #{field_ptr} but field length goes to #{ptr + 4 + field_name.length + field_l}" unless field_ptr == ptr + 4 + field_name.length + field_l
 
-	return field_value, ptr
+  ptr = ptr + 4 + field_name.length
+  field_value = ols[ptr..ptr + field_l - 1]
+  ptr = field_ptr
+
+  return field_value, ptr
 end
 
 def read_uk_field(ols, ptr, field_name)
+  field_ptr = readint(ols, ptr)
+  ptr += 4
+  raise "Not this field #{field_name} at #{ptr}!" unless ols[ptr..ptr + 3 + field_name.length] == [field_name.length].pack('l') + field_name
 
-	field_ptr = readint(ols, ptr)
-	ptr += 4
-	raise "Not this field #{field_name} at #{ptr}!" unless ols[ptr..ptr+3+field_name.length] == [field_name.length].pack('l')+field_name 
-	data = ols[ptr+4+field_name.length..field_ptr-1]
-	ptr = field_ptr
-	return data, ptr	
+  data = ols[ptr + 4 + field_name.length..field_ptr - 1]
+  ptr = field_ptr
+  return data, ptr
 end
 
 #----
 
 def parse_plate(ols, ptr)
+  begin
+    cover_batches = readint(ols, ptr)
+    ptr += 15
+    batch_num = readint(ols, ptr)
+    ptr += 4
+    # batch_num, ptr = read_int_field(ols, ptr, "Batches") This length check is then impossible. have to let the next field do that.
+    # puts "number of batches = #{batch_num}"
 
-	begin
+    batch = 1
+    batches = Array.new
+    while batch <= batch_num do
+      #	puts "trying to get batch number #{batch}"
+      owner, ptr = read_text_field(ols, ptr, "Owner")
+      id, ptr = read_text_field(ols, ptr, "ID")
+      logtimeraw, ptr = read_int_field(ols, ptr, "LogTime")
+      locations, ptr = read_uk_field(ols, ptr, "Locations")
+      priority, ptr = read_32b(ols, ptr, "Priority")
+      raise "Expecting EOO at #{ptr}!" if ptr != readint(ols, ptr) || ols[ptr + 4..ptr + 10] != "\x03\0\0\0EOO"
 
-	cover_batches = readint(ols, ptr)
-	ptr += 15
-	batch_num = readint(ols, ptr)
-	ptr +=4
-	#batch_num, ptr = read_int_field(ols, ptr, "Batches") This length check is then impossible. have to let the next field do that.
-	#puts "number of batches = #{batch_num}"
-	
-	batch = 1
-	batches = Array.new
-	while batch <= batch_num do
-	#	puts "trying to get batch number #{batch}"
-		owner, ptr = read_text_field(ols, ptr, "Owner")
-		id, ptr = read_text_field(ols, ptr, "ID")
-		logtimeraw, ptr = read_int_field(ols, ptr, "LogTime")
-		locations, ptr = read_uk_field(ols, ptr, "Locations")
-		priority, ptr = read_32b(ols, ptr, "Priority")
-		raise "Expecting EOO at #{ptr}!" if ptr != readint(ols, ptr) || ols[ptr+4..ptr+10] != "\x03\0\0\0EOO"
-		batches.push([owner, id, Time.strptime(logtimeraw.to_s, "%s"), locations, priority])
-		ptr += 11
-		batch += 1
-	end
-	raise "batch number don't agree #{batch_num} != #{batches.length}" unless batch_num == batches.length
-	#puts "after batch list ptr = #{ptr}"
+      batches.push([owner, id, Time.strptime(logtimeraw.to_s, "%s"), locations, priority])
+      ptr += 11
+      batch += 1
+    end
+    raise "batch number don't agree #{batch_num} != #{batches.length}" unless batch_num == batches.length
 
-	desc, ptr = read_text_field(ols, ptr, "Desc")
-	owner, ptr = read_text_field(ols, ptr, "Owner")
-	plate, ptr = read_uk_field(ols, ptr, "Plate")
-	excludes, ptr = read_32b(ols, ptr, "Excludes")
-	holder, ptr = read_32b(ols, ptr, "Holder")
-	usage, ptr = read_32b(ols, ptr, "Usage")
-	lastusedpos, ptr = read_32b(ols, ptr, "LastUsedPos")
-	
-	
-	raise "Expecting EOO at #{ptr}!" if ptr != readint(ols, ptr) || ols[ptr+4..ptr+10] != "\x03\0\0\0EOO"
-	ptr += 11
-	return batches, desc, owner, plate, excludes, holder, usage, ptr
-	end #begin
+    # puts "after batch list ptr = #{ptr}"
+
+    desc, ptr = read_text_field(ols, ptr, "Desc")
+    owner, ptr = read_text_field(ols, ptr, "Owner")
+    plate, ptr = read_uk_field(ols, ptr, "Plate")
+    excludes, ptr = read_32b(ols, ptr, "Excludes")
+    holder, ptr = read_32b(ols, ptr, "Holder")
+    usage, ptr = read_32b(ols, ptr, "Usage")
+    lastusedpos, ptr = read_32b(ols, ptr, "LastUsedPos")
+
+    raise "Expecting EOO at #{ptr}!" if ptr != readint(ols, ptr) || ols[ptr + 4..ptr + 10] != "\x03\0\0\0EOO"
+
+    ptr += 11
+    return batches, desc, owner, plate, excludes, holder, usage, ptr
+  end # begin
 end
 
 def parse_ols(path)
-	
-	eoo = "\x03\0\0\0EOO"
-	times = 0
-	begin	
-	fo = File.open(path, "rb")
-	ols = fo.read
-	fo.close #close the ols immediately
-	rescue
-		retry if (times += 1) < 2
-	end
-	begin
-	ptr = 0
-	cover_ASBedInfo = readint(ols, ptr)
-	raise "cover_ASBedInfo didn't close an EOO" unless ols[cover_ASBedInfo-7..cover_ASBedInfo-1] == eoo
-#	puts "cover_ASBedInfo = #{cover_ASBedInfo}"
-	ptr += 25
+  eoo = "\x03\0\0\0EOO"
+  times = 0
+  begin
+    fo = File.open(path, "rb")
+    ols = fo.read
+    fo.close # close the ols immediately
+  rescue
+    retry if (times += 1) < 2
+  end
+  begin
+    ptr = 0
+    cover_ASBedInfo = readint(ols, ptr)
+    raise "cover_ASBedInfo didn't close an EOO" unless ols[cover_ASBedInfo - 7..cover_ASBedInfo - 1] == eoo
 
-	batches_1, desc_1, owner_1, plate_str_1, excludes_1, holder_1, usage_1, ptr = parse_plate(ols, ptr)
-	batches_2, desc_2, owner_2, plate_str_2, excludes_2, holder_2, usage_2, ptr = parse_plate(ols, ptr)
-	
-	raise "cover_ASBedInfo = #{cover_ASBedInfo} but ptr after two plate = #{ptr}" unless cover_ASBedInfo == ptr
+    #	puts "cover_ASBedInfo = #{cover_ASBedInfo}"
+    ptr += 25
 
-	current, ptr = read_text_field(ols, ptr, "CurrentBatch")
-	batch_ext, ptr = read_text_field(ols, ptr, "BatchFileExt")
-	inj_state, ptr = read_32b(ols, ptr, "InjState")
-	server_status_cycle, ptr = read_32b(ols, ptr, "ServerStatusCycle")
-	oastat, ptr = read_32b(ols, ptr, "OAState")
-	last_err, ptr = read_text_field(ols, ptr, "LastError")
-	auto_smplr, ptr = read_text_field(ols, ptr, "AutoSampler")
-	analysis_time, ptr = read_32b(ols, ptr, "AnalysisTime")
-	analysis_time = analysis_time.unpack('f')[0]
-	invalid_plates, ptr = read_32b(ols, ptr, "InvalidPlates")
-	health, ptr = read_text_field(ols, ptr, "InstrumentHealthy")
+    batches_1, desc_1, owner_1, plate_str_1, excludes_1, holder_1, usage_1, ptr = parse_plate(ols, ptr)
+    batches_2, desc_2, owner_2, plate_str_2, excludes_2, holder_2, usage_2, ptr = parse_plate(ols, ptr)
 
-	raise "Expecting EOO at #{ptr}!" if ptr != readint(ols, ptr) || ols[ptr+4..ptr+10] != "\x03\0\0\0EOO"
+    raise "cover_ASBedInfo = #{cover_ASBedInfo} but ptr after two plate = #{ptr}" unless cover_ASBedInfo == ptr
 
-	return [batches_1, desc_1, owner_1, plate_str_1, excludes_1, holder_1, usage_1], [batches_2, desc_2, owner_2, plate_str_2, excludes_2, holder_2, usage_2], current, batch_ext, inj_state, server_status_cycle, oastat, last_err, auto_smplr, analysis_time, invalid_plates, health
+    current, ptr = read_text_field(ols, ptr, "CurrentBatch")
+    batch_ext, ptr = read_text_field(ols, ptr, "BatchFileExt")
+    inj_state, ptr = read_32b(ols, ptr, "InjState")
+    server_status_cycle, ptr = read_32b(ols, ptr, "ServerStatusCycle")
+    oastat, ptr = read_32b(ols, ptr, "OAState")
+    last_err, ptr = read_text_field(ols, ptr, "LastError")
+    auto_smplr, ptr = read_text_field(ols, ptr, "AutoSampler")
+    analysis_time, ptr = read_32b(ols, ptr, "AnalysisTime")
+    analysis_time = analysis_time.unpack('f')[0]
+    invalid_plates, ptr = read_32b(ols, ptr, "InvalidPlates")
+    health, ptr = read_text_field(ols, ptr, "InstrumentHealthy")
 
-	end #begin
-end #func parse_ols
+    raise "Expecting EOO at #{ptr}!" if ptr != readint(ols, ptr) || ols[ptr + 4..ptr + 10] != "\x03\0\0\0EOO"
+
+    return [batches_1, desc_1, owner_1, plate_str_1, excludes_1, holder_1, usage_1], [batches_2, desc_2, owner_2, plate_str_2, excludes_2, holder_2, usage_2], current, batch_ext, inj_state, server_status_cycle, oastat, last_err, auto_smplr, analysis_time, invalid_plates, health
+  end # begin
+end # func parse_ols
 
 class Machine
-	attr_reader :path, :name, :rpt_path
-	def initialize(name, ols_path, rpt_path)
-		@path = ols_path
-		@name = name
-		@rpt_path = rpt_path
-	end
+  attr_reader :path, :name, :rpt_path
+
+  def initialize(name, ols_path, rpt_path)
+    @path = ols_path
+    @name = name
+    @rpt_path = rpt_path
+  end
 end
 
 class Batch
-	attr_reader :batch_param, :plates, :samples
-	def initialize(olbpath)
-	
-@batch_param = {}
-@plates, @samples = [], []
-ptr = []
-fin = File.open(olbpath, "r:UTF-8")
-raw = fin.readlines
-fin.close
-current_plate=0
-begin
+  attr_reader :batch_param, :plates, :samples
 
-(0..raw.size-1).each do |ln|
-	if result = raw[ln].chomp.match(/^\[([\w]*):?(\d)?:?(\d)?\]$/) #if tag
-#	puts "matching tag #{result[0]}"
-		case result[1]
-		when "Batch"
-			ptr = @batch_param
-		#	puts "batch tag"
-		when "Sample"
-			@samples.push(Hash.new)
-			ptr = @samples.last
-			ptr["plate"], ptr["vial"] = result[2..3]
-			raise "plate # dosn't match with file #{olbpath} at line #{ln}, revise logic!" if ptr["plate"] != current_plate
-		when "Plate"
-			@plates.push(Hash.new)
-			ptr = @plates.last
-			ptr["index"] = current_plate = result[2]
-		end #end tag type
-	elsif result = raw[ln].chomp.match(/([^=]+)=([\w\W]*)/)
-		#puts "#{result[1]} === #{result[2]}"
-		ptr["#{result[1]}"] = result[2]
-	else
-		raise "Unrecognizable line: #{raw[ln]}"
-	end #if tag/line
-
-end
-rescue RuntimeError => err
-	case err.message
-	when /Unrecognizable\sline/
-		puts err
-	when /plate\s#/
-		puts err
-	else
-		raise "Fucked up"
-	end
-rescue ArgumentError => err
-	puts err
-	puts "in #{olbpath}"
-end
-	
-	end
+  def initialize(olbpath)
+    @batch_param = {}
+    @plates, @samples = [], []
+    ptr = []
+    fin = File.open(olbpath, "r:UTF-8")
+    raw = fin.readlines
+    fin.close
+    current_plate = 0
+    begin
+      (0..raw.size - 1).each do |ln|
+        if result = raw[ln].chomp.match(/^\[([\w]*):?(\d)?:?(\d)?\]$/) # if tag
+          #	puts "matching tag #{result[0]}"
+          case result[1]
+          when "Batch"
+            ptr = @batch_param
+          #	puts "batch tag"
+          when "Sample"
+            @samples.push(Hash.new)
+            ptr = @samples.last
+            ptr["plate"], ptr["vial"] = result[2..3]
+            raise "plate # dosn't match with file #{olbpath} at line #{ln}, revise logic!" if ptr["plate"] != current_plate
+          when "Plate"
+            @plates.push(Hash.new)
+            ptr = @plates.last
+            ptr["index"] = current_plate = result[2]
+          end # end tag type
+        elsif result = raw[ln].chomp.match(/([^=]+)=([\w\W]*)/)
+          # puts "#{result[1]} === #{result[2]}"
+          ptr["#{result[1]}"] = result[2]
+        else
+          raise "Unrecognizable line: #{raw[ln]}"
+        end # if tag/line
+      end
+    rescue RuntimeError => err
+      case err.message
+      when /Unrecognizable\sline/
+        puts err
+      when /plate\s#/
+        puts err
+      else
+        raise "Fucked up"
+      end
+    rescue ArgumentError => err
+      puts err
+      puts "in #{olbpath}"
+    end
+  end
 end
 
-#for visualization
+# for visualization
 
 def p_curve_plot(path, svg_out, n)
-	rpt_list = `find \"#{path}\" -maxdepth 1 -name \"*.rpt\" -mtime -6 -type f -printf '%Ts %f\n'| sort -nr|head -#{n}|cut -d ' ' -f2-`.split("\n")
-	fname_list = []
-	time_list = []
-	injection_volume_list = []
-	lcmethod_list = []
-	plot_data = File.new("data", "w")
-	rpt_list.each do |fname|
-		rpt = OALogin_Report.new(path+fname)
-		rpt.samples.reverse.each do |sample|
-			fname_list.push(sample.name)
-			time_list.push(sample.acqu_time)
-			injection_volume_list.push(sample.inject_volume)
-			lcmethod_list.push(sample.lc_method)	
-			sample.pressure_curve.each do |pressure_pt|
-				plot_data.puts "#{pressure_pt[0]}\t#{(pressure_pt[1].to_f)*sample.max_pressure/100}"
-			end
-			plot_data.write("\n\n")
-		end #end sample
-	end #end report iter
-		plot_data.close
+  rpt_list = `find \"#{path}\" -maxdepth 1 -name \"*.rpt\" -type f -printf '%Ts %f\n'| sort -nr|head -#{n}|cut -d ' ' -f2-`.split("\n")
+  fname_list = []
+  time_list = []
+  injection_volume_list = []
+  lcmethod_list = []
+  plot_data = File.new("data", "w")
+  rpt_list.each do |fname|
+    rpt = OALogin_Report.new(path + fname)
+    rpt.samples.reverse.each do |sample|
+      fname_list.push(sample.name)
+      time_list.push(sample.acqu_time)
+      injection_volume_list.push(sample.inject_volume)
+      lcmethod_list.push(sample.lc_method)
+      sample.pressure_curve.each do |pressure_pt|
+        plot_data.puts "#{pressure_pt[0]}\t#{(pressure_pt[1].to_f) * sample.max_pressure / 100}"
+      end
+      plot_data.write("\n\n")
+    end # end sample
+  end # end report iter
+  plot_data.close
 
-	gnuplot_command =<<"END"
-set terminal svg enhanced mouse jsdir './gnuplot_js/' size 1000 600
-set output "#{svg_out}"
-set xrange [0:7]
-set yrange[0:*]
-set key outside center bottom
-END
-	gnuplot_command  << "plot 'data' " 	
-	(0..fname_list.size-1).each do |fname_index|
-		gnuplot_command << ", '' " if fname_index > 0
-		gnuplot_command << "index #{fname_index} with lines t '#{fname_list[fname_index].split(/_(#{Time.now.strftime("%Y%m%d")}|#{(Time.now-86400).strftime("%Y%m%d")})/)[0].gsub('_','\_')} | #{time_list[fname_index].strftime("%R")} | #{lcmethod_list[fname_index].gsub('_','\_')} | #{injection_volume_list[fname_index]} uL'"
-	end
-	
-	image, s = Open3.capture2(
-		"gnuplot",
-		:stdin_data=> gnuplot_command, :binmode=>true)
-	
+  gnuplot_command = <<~"END"
+    set terminal svg enhanced mouse jsdir './gnuplot_js/' size 1000 600
+    set output "#{svg_out}"
+    set xrange [0:7]
+    set yrange[0:*]
+    set key outside center bottom
+  END
+  gnuplot_command << "plot 'data' "
+  (0..fname_list.size - 1).each do |fname_index|
+    gnuplot_command << ", '' " if fname_index > 0
+    gnuplot_command << "index #{fname_index} with lines t '#{fname_list[fname_index].split(/_(#{Time.now.strftime("%Y%m%d")}|#{(Time.now - 86400).strftime("%Y%m%d")})/)[0].gsub('_', '\_')} | #{time_list[fname_index].strftime("%R")} | #{lcmethod_list[fname_index].gsub('_', '\_')} | #{injection_volume_list[fname_index]} uL'"
+  end
+
+  image, s = Open3.capture2(
+    "gnuplot",
+    :stdin_data => gnuplot_command, :binmode => true
+  )
 end
-
--- product/main.rb
+++ product/main.rb
@@ -4,62 +4,60 @@ require './settings.rb'
 require 'open3'
 
 def get_machine(ols_path)
-	output = String.new
-	
-	#check ols_path
-	if ols_path =~/\/sq\d_c\/[\s\S]*\/Status.ols$/
-		sqnum = ols_path.split('/sq')[1].split('_c/')[0].to_i #MoBiAS name setup
-		path = ols_path.split('Status.ols')[0]
-	else #not standard MoBiAS environment, probably testing
-		sqnum = "UNDEF"
-		path = ols_path[0..(ols_path =~/\/[^\/]*$/)] # find the directory ... is this somewhat wheel inventing? whatever.
-	end
+  output = String.new
 
-	begin
-	ols_updtime = File.mtime(ols_path)
-	response = `cp "#{ols_path}" ./ols` #make a copy of the ols to avoid possible locking of the file
-	plate_1, plate_2, current, batch_ext, inj_state, server_status_cycle, oastat, last_err, auto_smplr, analysis_time, invalid_plates, health = parse_ols("./ols")
-	(0..plate_2[0].length-1).each do |i| #Set vial position - plate # to 2 if it's from plate 2
-		plate_2[0][i][3][0..3] = "\x02\0\0\0"
-	end
-	response = `rm ./ols` #remove ols temp to avoid ghost image from SQ1 to SQ3
-	batchlist = plate_1[0] + plate_2[0]
-	batchlist.sort! {|a, b| a[2] <=> b[2]} #sort with submission time
+  # check ols_path
+  if ols_path =~ /\/sq\d_c\/[\s\S]*\/Status.ols$/
+    sqnum = ols_path.split('/sq')[1].split('_c/')[0].to_i # MoBiAS name setup
+    path = ols_path.split('Status.ols')[0]
+  else # not standard MoBiAS environment, probably testing
+    sqnum = "UNDEF"
+    path = ols_path[0..(ols_path =~ /\/[^\/]*$/)] # find the directory ... is this somewhat wheel inventing? whatever.
+  end
 
-	deg=0
-	(0..batchlist.length-1).each do |i| #catch degeneracy when cross plate
-		if batchlist[i][1] == batchlist[i-1][1] && i > 1
-			deg += 1
-		end
-	end
+  begin
+    ols_updtime = File.mtime(ols_path)
+    response = `cp "#{ols_path}" ./ols` # make a copy of the ols to avoid possible locking of the file
+    plate_1, plate_2, current, batch_ext, inj_state, server_status_cycle, oastat, last_err, auto_smplr, analysis_time, invalid_plates, health = parse_ols("./ols")
+    (0..plate_2[0].length - 1).each do |i| # Set vial position - plate # to 2 if it's from plate 2
+      plate_2[0][i][3][0..3] = "\x02\0\0\0"
+    end
+    response = `rm ./ols` # remove ols temp to avoid ghost image from SQ1 to SQ3
+    batchlist = plate_1[0] + plate_2[0]
+    batchlist.sort! { |a, b| a[2] <=> b[2] } # sort with submission time
 
-	rescue
-		puts "Sth went wrong during ols file reading"
-		health = "FALSE"
-	end
+    deg = 0
+    (0..batchlist.length - 1).each do |i| # catch degeneracy when cross plate
+      if batchlist[i][1] == batchlist[i - 1][1] && i > 1
+        deg += 1
+      end
+    end
+  rescue
+    puts "Sth went wrong during ols file reading"
+    health = "FALSE"
+  end
 
+  if health == "FALSE"
+    output += "<div class=\"machinebox_red\"><table class=\"machinetable\" id =\"sq#{sqnum}\">\n<p class=\"m_table_title\">SQD#{sqnum}</p>\n"
+    output += "<p style=\"color: red;\">Not running properly.</p>\n"
+  else
+    output += "<div class=\"machinebox\"><table class=\"machinetable\" id =\"sq#{sqnum}\">\n<p class=\"m_table_title\">SQD#{sqnum}</p>\n"
+    if batchlist.length == 0
+      output += "<p>Machine has an empty queue.</p>\n"
+    end
+  end # if healthy
 
-	if health == "FALSE"
-	output += "<div class=\"machinebox_red\"><table class=\"machinetable\" id =\"sq#{sqnum}\">\n<p class=\"m_table_title\">SQD#{sqnum}</p>\n"
-		output += "<p style=\"color: red;\">Not running properly.</p>\n"
-	else 
-		output += "<div class=\"machinebox\"><table class=\"machinetable\" id =\"sq#{sqnum}\">\n<p class=\"m_table_title\">SQD#{sqnum}</p>\n"
-	if batchlist.length == 0
-		output += "<p>Machine has an empty queue.</p>\n"
-	end
-	end # if healthy
-
-		#report the queue
-		if batchlist.length-deg == 1
-			output += "<p> Has one job"
-		else
-			output += "<p> Has #{batchlist.length-deg} jobs"
-		end #Job number cases
-		mins = (analysis_time/60).to_int 
-		output += " in the queue, time needed: "
-		output += "#{mins} mins and " if mins > 0
-		output += "#{(analysis_time.to_int%60)} seconds.</p>"
-		output +="
+  # report the queue
+  if batchlist.length - deg == 1
+    output += "<p> Has one job"
+  else
+    output += "<p> Has #{batchlist.length - deg} jobs"
+  end # Job number cases
+  mins = (analysis_time / 60).to_int
+  output += " in the queue, time needed: "
+  output += "#{mins} mins and " if mins > 0
+  output += "#{(analysis_time.to_int % 60)} seconds.</p>"
+  output += "
 		<tr class=\"first_row\">
 		<td nowrap class =\"first_row\">Job name</td>
 		<td nowrap class =\"first_row\">Time submitted</td>
@@ -68,107 +66,95 @@ def get_machine(ols_path)
 		<td nowrap class =\"first_row\">Time(min)</td>
 		<td nowrap class =\"first_row\">Priority / Night queue</td></tr>"
 
-		batchlist.each do |batch| #batchlist loop
-			output += "<tr #{'id ="current"' if batch[1] == current}><td nowrap>#{batch[1]}</td>
+  batchlist.each do |batch| # batchlist loop
+    output += "<tr #{'id ="current"' if batch[1] == current}><td nowrap>#{batch[1]}</td>
 			<td nowrap class=\"internal\">#{batch[2]}</td>"
-			if batch[3].length == 12 #if in the plate and not bigger vial slots
-			positions = "Plate #{batch[3][0..3].unpack('l')[0].to_s} ##{batch[3][4..7].unpack('l')[0].to_s}"
-			if batch[3][4..7] != batch[3][8..11] #more than one? number_of_vials calc
-				begin
-					n_o_v = batch[3][8..11].unpack('l')[0] - batch[3][4..7].unpack('l')[0]+1 
-				rescue
-					n_o_v = nil
-				end
-					positions += "~#{batch[3][8..11].unpack('l')[0].to_s}"
-			else
-				n_o_v = 1
-			end #more than one
-			end #In 48 well plate
-			begin
-			olb = Batch.new(path+batch[1]+".OLB")
-			output += "<td nowrap class=\"internal\">#{positions}</td>"
-			#lcmethod = `grep 'LCMethod=' "#{path}/#{batch[1]}.OLB"`
-			#lcmethod = lcmethod.split('=')[1].chomp
-			lcmethod = olb.batch_param["LCMethod"]
-			output += "<td nowrap class=\"internal\">#{lcmethod}</td>"
-			#time_min = `grep 'AnalysisTime=' "#{path}/#{batch[1]}.OLB"`
-			#time_min = time_min.split('=')[1].chomp.to_f
-			time_min = olb.batch_param["AnalysisTime"].to_f
-			if time_min == nil
-				time_min = 0
-			end
-			output += "<td nowrap class=\"internal\">#{time_min * n_o_v if n_o_v.class != NilClass}</td>"
-			
-			output += "<td nowrap class=\"internal\">" #priority / night queue
-			if batch[4].unpack('l')[0] != 0
-				output += "Y / "
-			else
-				output += "- / "
-			end
-			#eco_sched = `grep 'EconomyScheduling=' "#{path}/#{batch[1]}.OLB"`.split('=')[1].to_i
-			eco_sched = olb.batch_param["EconomyScheduling"].to_i
-			if eco_sched == 1
-				output += "Y"
-			else
-				output += "-"
-			end
-			output +=  "</td></tr>"
-			rescue Exception => err
-			output += "<td nowrap class=\"internal\" colspan=\"4\">Batch file error!<!--#{err}--></td></tr>"
-			end
-		end #end batchlist loop
+    if batch[3].length == 12 # if in the plate and not bigger vial slots
+      positions = "Plate #{batch[3][0..3].unpack('l')[0].to_s} ##{batch[3][4..7].unpack('l')[0].to_s}"
+      if batch[3][4..7] != batch[3][8..11] # more than one? number_of_vials calc
+        begin
+          n_o_v = batch[3][8..11].unpack('l')[0] - batch[3][4..7].unpack('l')[0] + 1
+        rescue
+          n_o_v = nil
+        end
+        positions += "~#{batch[3][8..11].unpack('l')[0].to_s}"
+      else
+        n_o_v = 1
+      end # more than one
+    end # In 48 well plate
+    begin
+      olb = Batch.new(path + batch[1] + ".OLB")
+      output += "<td nowrap class=\"internal\">#{positions}</td>"
+      # lcmethod = `grep 'LCMethod=' "#{path}/#{batch[1]}.OLB"`
+      # lcmethod = lcmethod.split('=')[1].chomp
+      lcmethod = olb.batch_param["LCMethod"]
+      output += "<td nowrap class=\"internal\">#{lcmethod}</td>"
+      # time_min = `grep 'AnalysisTime=' "#{path}/#{batch[1]}.OLB"`
+      # time_min = time_min.split('=')[1].chomp.to_f
+      time_min = olb.batch_param["AnalysisTime"].to_f
+      if time_min == nil
+        time_min = 0
+      end
+      output += "<td nowrap class=\"internal\">#{time_min * n_o_v if n_o_v.class != NilClass}</td>"
 
+      output += "<td nowrap class=\"internal\">" # priority / night queue
+      if batch[4].unpack('l')[0] != 0
+        output += "Y / "
+      else
+        output += "- / "
+      end
+      # eco_sched = `grep 'EconomyScheduling=' "#{path}/#{batch[1]}.OLB"`.split('=')[1].to_i
+      eco_sched = olb.batch_param["EconomyScheduling"].to_i
+      if eco_sched == 1
+        output += "Y"
+      else
+        output += "-"
+      end
+      output += "</td></tr>"
+    rescue Exception => err
+      output += "<td nowrap class=\"internal\" colspan=\"4\">Batch file error!<!--#{err}--></td></tr>"
+    end
+  end # end batchlist loop
 
+  output += "</table><p>OALogin last sign of life detected at #{ols_updtime}</p>\n</div>"
 
-	output += "</table><p>OALogin last sign of life detected at #{ols_updtime}</p>\n</div>"
-	
-	return output
-
-end #def get_machine
-
+  return output
+end # def get_machine
 
 #---main
-output = <<-EOHeader
-<head>
-<title>#{$html_title}</title>
-<link rel="stylesheet" type="text/css" href="ui.css">
-<meta http-equiv="refresh" content="30">
-</head>
-
-<body>
-<!--header before-->
-
-<div id="pagebox">
+output = <<~EOHeader
+    <head>
+    <title>#{$html_title}</title>
+    <link rel="stylesheet" type="text/css" href="ui.css">
+    <meta http-equiv="refresh" content="30">
+    </head>
+  #{'  '}
+    <body>
+    <!--header before-->
+  #{'  '}
+    <div id="pagebox">
 EOHeader
 
-
 $machines.each do |machine|
-
-begin
-	puts "acquiring machine named #{machine.name} at ols path of #{machine.path}"
-	output += get_machine(machine.path)
-	puts "machine #{machine.name} acquired"
-rescue RuntimeError => err
-	#output += "<div class=\"machinebox_red\"><table class=\"machinetable\" id =\"#{machine.name}\">\n<p class=\"m_table_title\">#{machine.name} Canot be reached</p></div>\n"
-	puts err
-		
-rescue NoMethodError => err
-	output += "NoNethodErr: #{err}"
-rescue Exception => err
-	output += "Machine #{machine.name}'s got some other error: #{err}"
-end
-end #end machine
-
+  begin
+    puts "acquiring machine named #{machine.name} at ols path of #{machine.path}"
+    output += get_machine(machine.path)
+    puts "machine #{machine.name} acquired"
+  rescue RuntimeError => err
+    # output += "<div class=\"machinebox_red\"><table class=\"machinetable\" id =\"#{machine.name}\">\n<p class=\"m_table_title\">#{machine.name} Canot be reached</p></div>\n"
+    puts err
+  rescue NoMethodError => err
+    output += "NoNethodErr: #{err}"
+  rescue Exception => err
+    output += "Machine #{machine.name}'s got some other error: #{err}"
+  end
+end # end machine
 
 output += "<p>Page updated at #{Time.now}</p></div>"
 fo = File.open("#{$html_path}stat.html", "w")
 fo.puts output
 fo.close
 
-$machines.each do |machine| #plot p curves
-
-	p_curve_plot(machine.rpt_path, "#{$html_path+machine.name}.svg", 5)
-
+$machines.each do |machine| # plot p curves
+  p_curve_plot(machine.rpt_path, "#{$html_path + machine.name}.svg", 5)
 end
-
-
